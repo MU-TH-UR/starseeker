3ebcb9fcf8b02d9a39e401015f98a3ec8cb3b9c5
Sheldon Els <sels@blizzard.com>
2016-04-20 07:10:24 +0200

    indentation

--- src/categories.js
+++ src/categories.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
 module.exports = function(file, done){
-	fs.readFile(file, function (err, article) {
-	    if (err) throw err;
-		done(['Science Fiction', 'Fantasy']);
-	});
+    fs.readFile(file, function (err, article) {
+        if (err) throw err;
+        done(['Science Fiction', 'Fantasy']);
+    });
 }
